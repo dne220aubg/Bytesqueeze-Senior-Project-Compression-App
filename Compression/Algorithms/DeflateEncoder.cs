78bfb4c363bc2fa94306de2526b078dcf18cc528
dne220 <dne220@proton.me>
2025-12-04 14:52:19 +0200

    Minor structure updates

--- Compression/Algorithms/DeflateEncoder.cs
+++ Compression/Algorithms/DeflateEncoder.cs
@@ -23,11 +23,45 @@ namespace SeniorProjectCompressionApp.Compression.Algorithms
         private readonly int _niceMatch;
         private readonly bool _lazyMatching;
 
-        private static readonly int[] CodeLengthOrder = new int[] { 16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15 };
-        private static readonly int[] s_lengthBases = new int[] { 3,4,5,6,7,8,9,10,11,13,15,17,19,23,27,31,35,43,51,59,67,83,99,115,131,163,195,227,258 };
-        private static readonly int[] s_lengthExtras = new int[] { 0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,0 };
-        private static readonly int[] s_distanceBases = new int[] { 1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025,1537,2049,3073,4097,6145,8193,12289,16385,24577 };
-        private static readonly int[] s_distanceExtras = new int[] { 0,0,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13 };
+        private static readonly int[] CodeLengthOrder = new int[]
+        {
+            16, 17, 18, 0, 8, 7, 9, 6,
+            10, 5, 11, 4, 12, 3, 13, 2,
+            14, 1, 15
+        };
+
+        private static readonly int[] s_lengthBases = new int[]
+        {
+            3, 4, 5, 6, 7, 8, 9, 10,
+            11, 13, 15, 17, 19, 23, 
+            27, 31, 35, 43, 51, 59,
+            67, 83, 99, 115,131, 163,
+            195, 227, 258
+        };
+
+        private static readonly int[] s_lengthExtras = new int[]
+        {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 2, 2, 2, 2,
+            3, 3, 3, 3, 4, 4, 4, 4,
+            5, 5, 5, 5, 0
+        };
+
+        private static readonly int[] s_distanceBases = new int[]
+        {
+            1, 2, 3, 4, 5, 7, 9, 13,
+            17, 25, 33, 49, 65, 97, 129, 193,
+            257, 385, 513, 769, 1025, 1537, 2049, 3073,
+            4097, 6145, 8193, 12289, 16385, 24577
+        };
+
+        private static readonly int[] s_distanceExtras = new int[]
+        {
+            0, 0, 0, 0, 1, 1, 2, 2,
+            3, 3, 4, 4, 5, 5, 6, 6,
+            7, 7, 8, 8, 9, 9, 10, 10,
+            11, 11, 12, 12, 13, 13
+        };
 
         private readonly Stream _output;
         private readonly DeflateBitWriter _writer;
